66df93623417055e0073ec48da8f76791337f861
motatoes <moe.habib9@gmail.com>
2017-02-01 23:45:44 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Introduction
+==============
+
+This is a simple Flask app that implements a bot which when mentioned in groups would share a random media item based on a database of media vids and images.
---
+++ flaskapp.py
@@ -0,0 +1,25 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def incoming():
+    if not kik.verify_signature(request.headers.get('X-Kik-Signature'), request.get_data()):
+        return Response(status=403)
+
+    messages = messages_from_json(request.json['messages'])
+
+    for message in messages:
+        if isinstance(message, TextMessage):
+            kik.send_messages([
+                TextMessage(
+                    to=message.from_user,
+                    chat_id=message.chat_id,
+                    body=message.body
+                )
+            ])
+
+    return Response(status=200)
+
+
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
